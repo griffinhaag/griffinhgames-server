8b5a9558fd7912da62beb29279c50330e9a1798e
griffinhaag <griffinhaag123@gmail.com>
2025-11-21 15:30:19 -0600

    Removed example reflex and trivia games

--- games/index.js
+++ games/index.js
@@ -1,9 +1,10 @@
-import reflexGame from "./reflex/index.js";
-import triviaGame from "./trivia/index.js";
+// Game registry - add your multiplayer games here
+// Example:
+// import myGame from "./mygame/index.js";
+// const gameRegistry = {
+//   mygame: myGame
+// };
 
-const gameRegistry = {
-  reflex: reflexGame,
-  trivia: triviaGame
-};
+const gameRegistry = {};
 
 export default gameRegistry;
